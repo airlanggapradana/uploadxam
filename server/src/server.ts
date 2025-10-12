45c62c86be6cf909a72a84afbb7c036a1432604e
Airlangga Pradana <ranggavfxartwork@gmail.com>
2025-10-12 20:45:36 +0700

    feat: add rate limiting middleware to enhance API security

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,9 +4,15 @@ import {errorHandler} from "./middlewares/errorHandler";
 import userRouter from "./controllers/users.controller";
 import {env} from "./env";
 import authRouter from "./controllers/auth.controller";
+import {rateLimit} from 'express-rate-limit';
 
 const app: Application = express();
+const limiter = rateLimit({
+  windowMs: 5 * 60 * 1000, // 5 minutes
+  max: 100 // limit each IP to 100 requests per windowMs
+})
 
+app.use(limiter);
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
