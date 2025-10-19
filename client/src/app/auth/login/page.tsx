e31246b32fb71aff6721db9ab64cc95ea06d0b36
Airlangga Pradana <ranggavfxartwork@gmail.com>
2025-10-19 08:40:21 +0700

    feat: add metadata for login and registration pages in UploadXam

--- client/src/app/auth/login/page.tsx
+++ client/src/app/auth/login/page.tsx
@@ -8,6 +8,66 @@ import {
 } from "@/components/ui/card";
 import Link from "next/link";
 import LoginForm from "@/components/auth/LoginForm";
+import type { Metadata } from "next";
+import media from "../../../../public/media.png";
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://uploadxam.fostiums.org/auth/login"),
+  title: "UploadXam | Login Akun",
+  description:
+    "Merupakan solusi digital untuk mengelola soal-soal ujian mahasiswa dari semua prodi di FKI UMS.",
+  keywords: [
+    "UploadXam",
+    "Bank Soal Digital",
+    "FKI UMS",
+    "Soal Ujian",
+    "Digital Soal",
+    "Manajemen Soal",
+    "Prodi FKI UMS",
+  ],
+  authors: [{ name: "Airlangga Pradana" }, { name: "FOSTI UMS" }],
+  creator: "Airlangga Pradana",
+  publisher: "FOSTI UMS",
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://uploadxam.fostiums.org/auth/login",
+    title: "UploadXam | Bank Soal Digital FKI UMS",
+    description:
+      "Merupakan solusi digital untuk mengelola soal-soal ujian mahasiswa dari semua prodi di FKI UMS.",
+    images: [
+      {
+        url: media.src,
+        width: 1200,
+        height: 630,
+        alt: "UploadXam | Bank Soal Digital FKI UMS",
+      },
+    ],
+    siteName: "UploadXam",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "UploadXam | Bank Soal Digital FKI UMS",
+    description:
+      "Merupakan solusi digital untuk mengelola soal-soal ujian mahasiswa dari semua prodi di FKI UMS.",
+    images: [media.src],
+  },
+  alternates: {
+    canonical: "https://uploadxam.fostiums.org/auth/login",
+  },
+  icons: [{ rel: "icon", url: "/favicon.ico" }],
+};
 
 const Login = () => {
   return (
